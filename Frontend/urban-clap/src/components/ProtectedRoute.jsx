57e3b86862e96fac3f1f9b91e90d19e3c4e889f1
Arpit Tripathi <1061arpittripathi>
2025-12-27 14:30:58 +0530

    Fixed Circular Progress

--- Frontend/urban-clap/src/components/ProtectedRoute.jsx
+++ Frontend/urban-clap/src/components/ProtectedRoute.jsx
@@ -1,16 +1,22 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./AuthContext";
-import {CircularProgress} from "@mui/material"
+import { CircularProgress } from "@mui/material";
 
 const ProtectedRoute = () => {
   const { user, loading } = useAuth();
-    console.log("ProtectedRoute user:", user, "loading:", loading);
+  console.log("ProtectedRoute user:", user, "loading:", loading);
 
-if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <CircularProgress size={80} color="inherit" />
+      </div>
+    );
+  }
 
-if (!user) return <Navigate to="/login" replace />;
+  if (!user) return <Navigate to="/login" replace />;
 
-  return <Outlet/>;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
