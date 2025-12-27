e04866ce2c58b2318863ecf8085e464eba08d7dc
Arpit Tripathi <1061arpittripathi>
2025-12-27 13:45:55 +0530

    Fixed Backent to frontend connection

--- Frontend/urban-clap/src/axiosInstance.js
+++ Frontend/urban-clap/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API = import.meta.env.VITE_API_BASE_URL;
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: API,
   withCredentials: true, // 🔥 REQUIRED FOR COOKIES
 });
 
