75f9b007fd28273259ee8e7215e1183d000e86f6
Arpit Tripathi <1061arpittripathi>
2026-05-09 13:23:02 +0530

    Remove node modules

---
+++ Backend/payment/razorpay.js
@@ -0,0 +1,10 @@
+const Razorpay=require('razorpay');
+
+const razorpayInstance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZOR_KEY_SECRET
+})
+
+const makepayment=async(req,res)=>{
+    
+}
